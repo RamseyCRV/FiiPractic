8e72051761332cef3985021ba5567a365892e85d
Robert <robert96vasile@gmail.com>
2019-03-09 14:46:29 +0200

    first commit

---
+++ src/main/java/com/robert/bankTr/BankTrApplication.java
@@ -0,0 +1,13 @@
+package com.robert.bankTr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTrApplication.class, args);
+	}
+
+}
